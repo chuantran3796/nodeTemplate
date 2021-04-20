5b19b760e87a341c755df1d9af0b341544852789
chuantran3796 <chuantran3796@gmail.com>
2021-04-20 11:03:21 +0700

    update

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -2,8 +2,35 @@ export default function socket(app: any) {
   const server = require("https").createServer(app);
   const io = require("socket.io")(server);
   let broadcaster: any;
+  let id: any;
 
   io.on("connection", (socket: any) => {
+    socket
+      .on("init", async () => {
+        id = await users.create(socket);
+        socket.emit("init", { id });
+      })
+      .on("request", (data: any) => {
+        const receiver = users.get(data.to);
+        if (receiver) {
+          receiver.emit("request", { from: id });
+        }
+      })
+      .on("call", (data: any) => {
+        const receiver = users.get(data.to);
+        if (receiver) {
+          receiver.emit("call", { ...data, from: id });
+        } else {
+          socket.emit("failed");
+        }
+      })
+      .on("end", (data: any) => {
+        const receiver = users.get(data.to);
+        if (receiver) {
+          receiver.emit("end");
+        }
+      });
+
     socket.on("broadcaster", () => {
       broadcaster = socket.id;
       socket.broadcast.emit("broadcaster");
